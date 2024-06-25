8fe9a1043fcf3a4fbf33e42c8a85b769aa468b30
muhumbania <franklinmuhumbania@gmail.com>
2024-06-25 23:55:09 +0300

    Adding the register face

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const inputs = document.querySelectorAll('.input');
 const register = document.getElementById('register');
+const position = false;
 
 inputs.forEach(function(input){
     input.addEventListener('focus', function(){
@@ -21,5 +22,28 @@ inputs.forEach(function(input){
 });
 
 register.addEventListener('click', function(){
-    document.getElementById('main').style.transform = "rotateY(180deg)";
+    
 });
+
+register.addEventListener('click', function(){
+    document.getElementById('main').style.transform = "rotateY(180deg)";
+    position = !position;
+
+    if(position){
+
+        Yrotation("none", "inline", "hide details", "rotateY(180deg)");
+    }else{
+
+        Yrotation("inline", "none", "show details", "rotateY(0deg)");
+    }
+});
+
+function Yrotation(hiddenView, showedView, legend, degree){
+    setTimeout(function(){
+        card.style.transform = degree;
+        document.querySelector('.hidden-visa').style.display = hiddenView;
+        document.querySelector('.showed-visa').style.display = showedView;
+        showText.innerHTML = legend;
+        button.setAttribute('name', legend.substring(0, 4));
+    }, 500);
+}
